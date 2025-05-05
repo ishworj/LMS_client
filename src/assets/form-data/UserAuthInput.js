9cc797554fc0b4c1a7498e6863373eb232389924
Ishwor karki <134608823+ishworj@users.noreply.github.com>
2025-05-05 22:33:30 +1000

    Update UserAuthInput.js
    using regex pattern

--- src/assets/form-data/UserAuthInput.js
+++ src/assets/form-data/UserAuthInput.js
@@ -26,6 +26,9 @@ export const userSignupInput = [
     required: true,
     placeholder: "Enter your password",
     type: "password",
+     pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$",
+  title:
+    "Password must be at least 8 characters, include an uppercase letter, a lowercase letter, a number, and a special character.",
   },
   {
     label: "Phone",
@@ -33,6 +36,8 @@ export const userSignupInput = [
     required: true,
     placeholder: "Enter your phone number",
     type: "tel",
+      pattern: "^((\\+61|0)4\\d{8})$",
+  title: "Enter a valid Australian mobile number starting with +61 or 04 followed by 8 digits",
   },
 ];
 
